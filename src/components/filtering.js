31d184633f094fa021a842cfb9f587000bc036c0
Vlad Makeev <vladmccain@yandex.ru>
2026-09-10 23:35:06 +0300

    первая попытка пройти автотесты

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,41 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes) // Получаем ключи из объекта
+    .forEach((elementName) => {
+      // Перебираем по именам
+      elements[elementName].append(
+        // в каждый элемент добавляем опции
+        ...Object.values(indexes[elementName]) // формируем массив имён, значений опций
+          .map((name) => {
+            // используйте name как значение и текстовое содержимое
+            // @todo: создать и вернуть тег опции
+            const option = document.createElement("option");
+            option.value = name;
+            option.textContent = name;
+            return option;
+          }),
+      );
+    });
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+    if (action && action.name === "clear") {
+      const fieldName = action.dataset.field;
+      const input = action.parentElement.querySelector("input");
+      if (input) {
+        input.value = "";
+      }
+      if (fieldName) {
+        state[fieldName] = "";
+      }
     }
+
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+    return data.filter(row => compare(row, state));
+  };
 }
